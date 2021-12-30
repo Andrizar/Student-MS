504184f56ac22fae3cecd1acd1f262a0ab65b764
Andrew Rexum Ssekirime <andrizar2@gmail.com>
2021-12-30 17:11:12 +0300

    Add db file

---
+++ schoolDB.sql
@@ -0,0 +1,251 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 30, 2021 at 03:06 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `schooldb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `class`
+--
+
+CREATE TABLE `class` (
+  `id` int(255) NOT NULL,
+  `class` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `class`
+--
+
+INSERT INTO `class` (`id`, `class`) VALUES
+(1, 'P1'),
+(2, 'P2'),
+(3, 'P3'),
+(4, 'P4'),
+(5, 'P5'),
+(6, 'P6'),
+(7, 'P7');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `marks`
+--
+
+CREATE TABLE `marks` (
+  `id` int(255) NOT NULL,
+  `sID` int(255) DEFAULT NULL,
+  `subjectID` int(255) DEFAULT NULL,
+  `mark` int(255) DEFAULT NULL,
+  `GRADE` int(255) NOT NULL,
+  `Remark` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `marks`
+--
+
+INSERT INTO `marks` (`id`, `sID`, `subjectID`, `mark`, `GRADE`, `Remark`) VALUES
+(1, 6, 1, 80, 0, 'Excellent'),
+(2, 7, 2, NULL, 0, NULL),
+(3, 10, 4, NULL, 0, NULL),
+(4, 6, NULL, NULL, 0, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE `students` (
+  `sID` int(255) NOT NULL,
+  `FirstName` varchar(255) NOT NULL,
+  `LastName` varchar(255) NOT NULL,
+  `regNo` varchar(255) NOT NULL,
+  `gender` varchar(255) NOT NULL,
+  `age` varchar(255) NOT NULL,
+  `class` varchar(255) NOT NULL,
+  `password` varchar(255) DEFAULT '12345'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`sID`, `FirstName`, `LastName`, `regNo`, `gender`, `age`, `class`, `password`) VALUES
+(6, 'Buhwa123', 'Ezra', '182344', 'Male', '10', '4', '12345'),
+(7, 'Atwiine', 'Ryan', '189990', 'Male', '5', '3', '12345'),
+(10, 'Awori', 'Vivian', '1899911', 'Female', '7', '2', '12345');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subject`
+--
+
+CREATE TABLE `subject` (
+  `subjectID` int(255) NOT NULL,
+  `subjectName` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `subject`
+--
+
+INSERT INTO `subject` (`subjectID`, `subjectName`) VALUES
+(1, 'MATH'),
+(2, 'SCIENCE'),
+(3, 'SST'),
+(4, 'ENGLISH');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `timetable`
+--
+
+CREATE TABLE `timetable` (
+  `id` int(255) NOT NULL,
+  `classID` int(255) NOT NULL,
+  `teacherID` int(255) NOT NULL,
+  `subjectID` int(255) NOT NULL,
+  `DAY` varchar(255) NOT NULL,
+  `Time_Start` varchar(255) NOT NULL,
+  `Time_End` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `timetable`
+--
+
+INSERT INTO `timetable` (`id`, `classID`, `teacherID`, `subjectID`, `DAY`, `Time_Start`, `Time_End`) VALUES
+(1, 4, 5, 4, 'Monday', '10:00am', '12:00pm');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `teacherID` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `username`, `password`, `teacherID`) VALUES
+(5, 'Buhwa Erasmus Ezra', 'Ezra', '[C@74077ad2', 'TID123'),
+(6, 'Awori Vivian Maria', 'Vivian', '[C@75a501ce', 'TID1234'),
+(7, 'Atwiine Ryan Agaba', 'Ryan', '[C@6a9d6bf5', 'TID12345'),
+(8, 'Ssekirime Andrew', 'Andrew', '[C@48afa338', 'TID123456');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `class`
+--
+ALTER TABLE `class`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `marks`
+--
+ALTER TABLE `marks`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`sID`);
+
+--
+-- Indexes for table `subject`
+--
+ALTER TABLE `subject`
+  ADD PRIMARY KEY (`subjectID`);
+
+--
+-- Indexes for table `timetable`
+--
+ALTER TABLE `timetable`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `class`
+--
+ALTER TABLE `class`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `marks`
+--
+ALTER TABLE `marks`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `students`
+--
+ALTER TABLE `students`
+  MODIFY `sID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `subject`
+--
+ALTER TABLE `subject`
+  MODIFY `subjectID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `timetable`
+--
+ALTER TABLE `timetable`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
